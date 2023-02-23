30f6456716f37ed23ca8e735baad72077c98365b
Gonzalo <ggarciarivera09@gmail.com>
2023-02-23 16:41:53 -0500

    change

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
@@ -13,6 +14,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
