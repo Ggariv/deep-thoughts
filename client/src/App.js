ccfa194845fec60a9d2a251ec5fae214ab623f50
Gonzalo <ggarciarivera09@gmail.com>
2023-02-24 22:32:27 -0500

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -14,8 +15,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
   });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   });
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,28 +1,30 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
-
+  const [login, { error }] = useMutation(LOGIN_USER);
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
-
     setFormState({
       ...formState,
       [name]: value,
-    });
-  };
-
+      });
+    };
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
-  };
+    try {
+      const { data } = await login({ variables: { ...formState } });
+      Auth.login(data.login.token);
+      } 
+    catch (e) {
+      console.error(e);
+      }
+    };
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -53,6 +55,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
